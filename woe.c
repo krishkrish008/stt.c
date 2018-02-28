474ea1336bb8a2f46e5171699f2f998ad0caad51
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-02-28 15:44:03 +0530

    Create woe.c

---
+++ woe.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char s[300];
+    int c=0, i;
+ 
+    printf("enter the string\n");
+    scanf("%[^\n]s", s);
+    for (i=0;s[i]!='\0';i++)
+    {
+        if (s[i] == ' ')
+            c++;    
+    }
+    printf("number of words in given string are: %d\n", c+1);
+return 0;
+}
