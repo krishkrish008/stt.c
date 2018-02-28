f4f14b8c27677f5f2701831bc295ef2bda93fe60
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-02-28 16:32:40 +0530

    Create rangeeee.c

---
+++ rangeeee.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\nenter number");
+scanf("%d",&a);
+if(a>=1&&a<=10)
+printf("\nLies within the given range");
+else
+printf("\nNot in given range");
+getch();
+}
