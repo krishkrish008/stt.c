b50f6da7bd640413d0e35dc675543d14d9a224cc
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-03-02 16:00:54 +0530

    Create sdsdsd.c

---
+++ sdsdsd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char s[100];
+int k,i;
+clrscr();
+printf("\nEnter string");
+scanf("%s",s);
+printf("\nEnter how many times string has to be displayed");
+scanf("%d",&k);
+for(i=0;i<b;i++)
+{
+printf("\n%s",s);
+}
+getch();
+}
