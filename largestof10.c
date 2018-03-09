7b13e6728345e6e1e93c78e0a4b472733df69f01
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-03-09 16:19:04 +0530

    Create largestof10.c

---
+++ largestof10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main()
+ {
+ int a, b, c, d, e, f, g, h, i, j, p, q, w, x, y, z, largestnumber;
+ clrscr();
+  printf(" \nEnter Ten numbers  ") ;
+  scanf("%d %d %d %d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j) ;
+   x = (a > b? a : b);
+   y = (c > d? c : d);
+   z = (e > f? e : f);
+   p = (g > h? g : h);
+   q = (i > j? i : j);
+   w = x > y ? ( x > z ? x : z) : (y > z ? y : z) ;
+   largestnumber = p > q ? ( p > w ? p : w) : (q > w ? q : w) ;
+  printf("\n\n The largest of Ten numbers will be %d", largestnumber) ;
+ getch();
+   }
