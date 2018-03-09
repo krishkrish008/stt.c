3291b846b2eb62d6dd731b56ce0f9c5ac717a0ad
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-03-09 16:34:43 +0530

    Create mulof7.c

---
+++ mulof7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\nEnter the value of a");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("\nit is Multiples of 7");
+}
+else
+{
+printf("\nIt is not a multiple of 7");
+}
+getch();
+}
