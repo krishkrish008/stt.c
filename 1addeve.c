2f6327e8dba265119266cc5a38096182dd525e0e
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-03-12 04:05:49 -0700

    Create 1addeve.c

---
+++ 1addeve.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int M,N,a;
+printf("\nEnter the value of M");
+scanf("%d",&M);
+printf("\nEnter the value of N");
+scanf("%d",&N);
+a=(M+N);
+if(a%2==0)
+printf("\nEven");
+else
+printf("\nOdd");
+getch();
+}
